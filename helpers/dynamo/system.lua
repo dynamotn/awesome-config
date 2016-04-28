2017cd3114ff00ac43bf0db7a2eab51f9d78e20b
Đức Nam Trần <dynamo.foss@gmail.com>
2016-04-28 20:34:40 +0700

    Change behavior confirm
    I use Enter instead of Yes/No

--- helpers/dynamo/system.lua
+++ helpers/dynamo/system.lua
@@ -2,82 +2,57 @@
 local html = require("html")
 -- {{{ Lock function when using LightDM
 dynamo.lock = function()
-    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Khóa màn hình (gõ 'y' hoặc 'c' để xác nhận)? ") },
+    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Khóa màn hình (gõ 'Enter' để xác nhận)? ") },
                      mypromptbox[mouse.screen].widget,
                      function (t)
-                         if string.lower(t) == 'y' or string.lower(t) == 'c' then
-                             awful.util.spawn("dm-tool switch-to-greeter")
-                         end
-                     end,
-                     function (t, p, n)
-                         return awful.completion.generic(t, p, n, { 'c', 'k' })
+                         awful.util.spawn("dm-tool switch-to-greeter")
                      end)
 end
 -- }}}
 
 -- {{{ Quit function when using GNOME session
 dynamo.quit = function()
-    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Thoát phiên (gõ 'y' hoặc 'c' để xác nhận)? ") },
+    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Thoát phiên (gõ 'Enter' để xác nhận)? ") },
                      mypromptbox[mouse.screen].widget,
                      function (t)
-                         if string.lower(t) == 'y' or string.lower(t) == 'c' then
-                             awesome.emit_signal("exit", nil)
-                             if os.getenv("DESKTOP_SESSION") == "awesome-gnome" then
-                                 awful.util.spawn("gnome-session-quit --logout")
-                             else
-                                 awesome.quit()
-                             end
+                         awesome.emit_signal("exit", nil)
+                         if os.getenv("DESKTOP_SESSION") == "awesome-gnome" then
+                             awful.util.spawn("gnome-session-quit --logout")
+                         else
+                             awesome.quit()
                          end
-                     end,
-                     function (t, p, n)
-                         return awful.completion.generic(t, p, n, { 'c', 'k' })
                      end)
 end
 -- }}}
 
 -- {{{ Hibernate using systemd
 dynamo.hibernate = function()
-    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Ngủ đông (gõ 'y' hoặc 'c' để xác nhận)? ") },
+    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Ngủ đông (gõ 'Enter' để xác nhận)? ") },
                      mypromptbox[mouse.screen].widget,
                      function (t)
-                         if string.lower(t) == 'y' or string.lower(t) == 'c' then
-                             awful.util.spawn("systemctl hibernate")
-                         end
-                     end,
-                     function (t, p, n)
-                         return awful.completion.generic(t, p, n, { 'c', 'k' })
+                         awful.util.spawn("systemctl hibernate")
                      end)
 end
 -- }}}
 
 -- {{{ Reboot
 dynamo.reboot = function()
-    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Khởi động lại (gõ 'y' hoặc 'c' để xác nhận)? ") },
+    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Khởi động lại (gõ 'Enter' để xác nhận)? ") },
                      mypromptbox[mouse.screen].widget,
                      function (t)
-                         if string.lower(t) == 'y' or string.lower(t) == 'c' then
-                             awesome.emit_signal("exit", nil)
-                             awful.util.spawn("reboot")
-                         end
-                     end,
-                     function (t, p, n)
-                         return awful.completion.generic(t, p, n, { 'c', 'k' })
+                         awesome.emit_signal("exit", nil)
+                         awful.util.spawn("reboot")
                      end)
 end
 -- }}}
 
 -- {{{ Shutdown
 dynamo.shutdown = function()
-    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Tắt máy (gõ 'y' hoặc 'c' để xác nhận)? ") },
+    awful.prompt.run({ prompt = html(beautiful.fg_confirm, "Tắt máy (gõ 'Enter' để xác nhận)? ") },
                      mypromptbox[mouse.screen].widget,
                      function (t)
-                         if string.lower(t) == 'y' or string.lower(t) == 'c' then
-                             awesome.emit_signal("exit", nil)
-                             awful.util.spawn("shutdown -P now")
-                         end
-                     end,
-                     function (t, p, n)
-                         return awful.completion.generic(t, p, n, { 'c', 'k' })
+                         awesome.emit_signal("exit", nil)
+                         awful.util.spawn("shutdown -P now")
                      end)
 end
 -- }}}
