d1570ece87ea7897e65cdd51ad403a4aaddd552b
Đức Nam Trần <dynamo.foss@gmail.com>
2016-05-03 14:50:28 +0700

    Alt+Tab to switch windows

--- keyboard.lua
+++ keyboard.lua
@@ -49,7 +49,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
     awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
-    awful.key({ modkey,           }, "Tab",
+    awful.key({ altkey,           }, "Tab",
         function ()
             awful.client.focus.history.previous()
             if client.focus then
