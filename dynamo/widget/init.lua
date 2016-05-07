9de54d60ac423ec451144aea8f7cb6059c07bd99
Đức Nam Trần <dynamo.foss@gmail.com>
2016-05-08 00:02:32 +0700

    Change initialization from array to setmetatable

--- dynamo/widget/init.lua
+++ dynamo/widget/init.lua
@@ -1,3 +1,9 @@
-return {
-    label = require("dynamo.widget.label")
-}
+local setmetatable = setmetatable
+
+return setmetatable(
+        { _NAME = "dynamo.widget" },
+        { __index = function(table, key)
+                local module = rawget(table, key)
+                return module or require(table._NAME .. '.' .. key)
+        end }
+)
