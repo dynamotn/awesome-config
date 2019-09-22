c0479e7d370d592bb477edc777329248da716123
Trần Đức Nam <dynamo.foss@gmail.com>
2019-09-23 00:07:32 +0700

    Add debug function

--- global/debug.lua
+++ global/debug.lua
@@ -1,5 +1,7 @@
 -- Notification library
 local naughty = require("naughty")
+-- Transform Lua value into a human-readable representation
+local inspect = require("global.inspect")
 
 -- { Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -27,5 +29,21 @@ do
       })
     in_error = false
   end)
+end
+-- }
+
+-- { Useful debug function
+function dbg(vars, notify)
+  local vars = vars or ""
+  local notify = notify or false
+  local text = inspect(vars)
+  if notify then
+    naughty.notify({ text = text, timeout = 10 })
+  else
+    local file = io.open(".awesome_dbg", "a")
+    io.output(file)
+    io.write(text .. '\n')
+    io.close(file)
+  end
 end
 -- }
