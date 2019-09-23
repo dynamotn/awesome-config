3ab0b54e1a5e5664060ebab0eb03f9d5cb80635c
Trần Đức Nam <dynamo.foss@gmail.com>
2019-09-23 10:48:28 +0700

    Remove hotkeys popup for other apps

--- rc.lua
+++ rc.lua
@@ -12,9 +12,6 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
 
 -- { Custom library
 -- Error handling, variable definitions, pre-setup
