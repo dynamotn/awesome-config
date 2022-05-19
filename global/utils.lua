a9b24e5685006cb7b9d44d72072676fe125e8302
Trần Đức Nam <dynamo.foss@gmail.com>
2022-05-20 04:50:50 +0700

    refactor: move 3rd modules to vendor folder

--- global/utils.lua
+++ global/utils.lua
@@ -2,6 +2,12 @@
 os.setlocale(os.getenv("LANG"))
 -- }
 
+-- { Vendor modules
+local awful = require('awful')
+local local_rc_path = awful.util.get_configuration_dir()
+package.path = package.path .. ';' .. local_rc_path .. 'vendor/?.lua;' .. local_rc_path .. 'vendor/?/init.lua'
+-- }
+
 -- { Reconfigure notification
 local naughty = require("naughty")
 naughty.config.defaults.timeout = 10
