bf98c70840335065475c24c8e460516e5e02ca2f
Trần Đức Nam <dynamo.foss@gmail.com>
2022-08-04 17:56:41 +0700

    refactor: move linux_distribution to bootstrap module

--- bootstrap/variables.lua
+++ bootstrap/variables.lua
@@ -1,5 +1,10 @@
 -- AwesomeWM standard library
 local awful = require('awful')
+-- Custom library
+local misc = require('dynamo.misc')
 
 -- AwesomwWM local configuration path
 _G.local_rc_path = awful.util.get_configuration_dir()
+
+-- Linux distribution name
+_G.linux_distribution = misc.linux_distribution()
--- global/vars.lua
+++ global/vars.lua
@@ -1,7 +1,1 @@
--- Get Linux distribution name
-local misc = require('dynamo.misc')
-linux_distribution = misc.linux_distribution()
-
--- { Popup config
 number_of_processes = 25
--- }
