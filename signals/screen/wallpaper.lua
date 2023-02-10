d64015a0740e2cae862c2b82edb7ff8e57f73595
Trần Đức Nam <dynamo.foss@gmail.com>
2023-02-10 13:23:55 +0700

    feat: wallpaper on multiple screen

--- signals/screen/wallpaper.lua
+++ signals/screen/wallpaper.lua
@@ -1,5 +1,3 @@
--- AwesomeWM standard library
-local gears = require('gears')
 -- Bling library
 local bling = require('bling')
 -- Custom library
@@ -7,15 +5,18 @@ local filesystem = require('lib.filesystem')
 
 -- Setup wallpaper
 local wallpaper_folder = filesystem.xdg_user_dirs('PICTURES') .. '/Wallpaper'
-bling.module.wallpaper.setup({
-  set_function = bling.module.wallpaper.setters.simple_schedule,
-  wallpaper = {
-    ['08:00:00'] = { wallpaper_folder .. '/day', wallpaper_folder .. '/all' },
-    ['18:00:00'] = { wallpaper_folder .. '/night', wallpaper_folder .. '/all' },
-  },
-  schedule_set_function = bling.module.wallpaper.setters.random,
-  position = 'maximized',
-  ignore_aspect = true,
-  recursive = false,
-  change_timer = 60,
-})
+screen.connect_signal('request::wallpaper', function(s)
+  bling.module.wallpaper.setup({
+    screen = s,
+    set_function = bling.module.wallpaper.setters.simple_schedule,
+    wallpaper = {
+      ['08:00:00'] = { wallpaper_folder .. '/day', wallpaper_folder .. '/all' },
+      ['18:00:00'] = { wallpaper_folder .. '/night', wallpaper_folder .. '/all' },
+    },
+    schedule_set_function = bling.module.wallpaper.setters.random,
+    position = 'maximized',
+    ignore_aspect = true,
+    recursive = false,
+    change_timer = 60,
+  })
+end)
